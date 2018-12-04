ea67bbcf95b45d6395ce3d76023e7a456fd68f76
Claudes Gomes <claudesferreira99@gmail.com>
2018-12-04 20:55:14 -0300

    modelo físico BD completo

--- MyHotel/myhotel.sql
+++ MyHotel/myhotel.sql
@@ -1,53 +1,125 @@
 create table administrador (
-	id_adm INT AUTO INCREMENT PRIMARY KEY,
+	id_adm INT AUTO_INCREMENT PRIMARY KEY,
 	cpf_adm VARCHAR(14) UNIQUE,
 	senha VARCHAR(10) NOT NULL,
 	nome_adm VARCHAR(25) NOT NULL
 );
 
 create table funcionario (
-	id_func INT AUTO INCREMENT PRIMARY KEY,
+	id_func INT AUTO_INCREMENT PRIMARY KEY,
 	cpf_func VARCHAR(14) UNIQUE,
 	senha VARCHAR(10) NOT NULL,
 	nome_func VARCHAR(25) NOT NULL
 );
 
+-- TABELA DE LIGAÇÃO ENTRE ADM E FUNCIONARIO
+create table cadastra(
+	id_func INT,
+	id_adm INT,
+	FOREIGN KEY (id_func) REFERENCES funcionario(id_func),
+	FOREIGN KEY (id_adm) REFERENCES administrador(id_adm)
+);
+
+
+
 create table quarto(
-	id_quarto INT AUTO INCREMENT PRIMARY KEY,
+	id_quarto INT AUTO_INCREMENT PRIMARY KEY,
 	num_quarto INT NOT NULL,
 	andar_quarto INT NOT NULL,
 	preco_diaria DOUBLE NOT NULL,
 	preco_mensa DOUBLE NOT NULL,
-	qtd_banheiro INT NOT NULL,
-
+	qtd_banheiro INT NOT NULL
 );
 
 create table servico (
-	id_servico INT AUTO INCREMENT PRIMARY KEY,
+	id_servico INT AUTO_INCREMENT PRIMARY KEY,
 	nome_servico VARCHAR(20) NOT NULL,
-	preco_servico DOUBLE NOT NULL,
+	preco_servico DOUBLE NOT NULL
+);
+
+
+create table reserva(
+
+	id_reserva INT AUTO_INCREMENT PRIMARY KEY,
+	id_func INT,
+	id_cliente INT,
+	id_quarto INT,
+	id_servico INT,
+	data_saída DATE NOT NULL,
+	data_entrada DATE NOT NULL,
+	pagamento DOUBLE NOT NULL
 );
 
 create table cliente(
-	id_cliente INT AUTO INCREMENT PRIMARY KEY,
-	cpf_cliente VARCHAR(14) UNIQUE NO 	NULL,
+	id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+	cpf_cliente VARCHAR(14) UNIQUE NOT NULL,
 	nome_cliente VARCHAR(25) NOT NULL,
 	id_reserva INT,
 	data_nasc DATE NOT NULL,
 	email varchar(50) NOT NULL,
+	FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva)
+);
+-- tabela que liga funcionario efetuando reserva
+
+create table efetua(
+	id_reserva INT,
+	id_func INT,
+	FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva),
+	FOREIGN KEY (id_func) REFERENCES funcionario(id_func)
 );
 
-create table reserva(
+/*tabela que liga funcionario ao cliente ou seja check in*/
+	
+create table check_in (
+	id_cliente INT,
+	id_func INT,
+	FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+	FOREIGN KEY (id_func) REFERENCES funcionario(id_func)
+);
 
-id_reserva INT AUTO INCREMENT PRIMARY KEY,
-id_func INT NOT NULL UNIQUE,
-id_cliente INT NOT NULL UNIQUE,
-id_quarto INT NOT NULL UNIQUE,
-id_servico INT NOT NULL UNIQUE,
-data_saída DATE NOT NULL,
-data_entrada DATE NOT NULL,
-pagamento DOUBLE NOT NULL,
+-- ligacao de reservas com servicos
+
+create table contem(
+	id_servico INT,
+	id_reserva INT,
+	FOREIGN KEY (id_servico) REFERENCES servico(id_servico),
+	FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva)
+	
+);
+
+-- ligacao de reserva com cliente 
+create table tem(
+	id_cliente INT,
+	id_reserva INT,
+	FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+	FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva)
+	
+);
+	
+-- ligacao de reserva com quarto
+create table possui(
+	id_quarto INT,
+	id_reserva INT,
+	FOREIGN KEY (id_quarto) REFERENCES quarto(id_quarto),
+	FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva)
 
 );
 
+create table telefone(
+	id_cliente INT,
+	id_telefone INT UNIQUE NOT NULL,
+	telene_um VARCHAR(10),
+	telefone_dois VARCHAR(10),
+	FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)	
+	
+);
 
+ALTER TABLE cliente
+ADD FOREIGN KEY (id_reserva) REFERENCES reserva(id_reserva);
+
+ALTER TABLE reserva
+
+ADD FOREIGN KEY (id_func) REFERENCES funcionario(id_func),
+ADD	FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente),
+ADD	FOREIGN KEY (id_quarto) REFERENCES quarto(id_quarto),
+ADD	FOREIGN KEY (id_servico) REFERENCES servico(id_servico);
